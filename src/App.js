28dd5cc05245a103ee4604cc9ea8e1e732c29a0d
hassanShakur <hass.shakurhassan@gmail.com>
2023-02-01 08:58:10 +0300

    Basic init login requirements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import jwtDecode from 'jwt-decode';
+
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({});
+  const handleGoogleCallbackResponse = (response) => {
+    console.log('Encoded JWT ID token: ' + response.credential);
+    var userObject = jwtDecode(response.credential);
+    //TODO USE REDUX TO SET USER
+    setUser(userObject);
+    console.log(userObject);
+  };
+
+  useEffect(() => {
+    /*global google*/
+    google.accounts.id.initialize({
+      client_id:
+        '956329766193-cdgv0o9gv6vtf1o1166l0fhnf72hudg4.apps.googleusercontent.com',
+      callback: handleGoogleCallbackResponse,
+    });
+    google.accounts.id.renderButton(
+      document.getElementById('sign-in-btn'),
+      {
+        theme: 'outline',
+        size: 'large',
+      }
+    );
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='sign-in'>
+        <button id='sign-in-btn'></button>
+        {user && (
+          <div>
+            <img src={user.picture} alt='Profile pic' />
+            <h4>{user.name}</h4>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
