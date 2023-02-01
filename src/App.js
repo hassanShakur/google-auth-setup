76e6057e2fbbfddb4a7d7947bd044b783cedcbcb
hassanShakur <hass.shakurhassan@gmail.com>
2023-02-01 09:49:43 +0300

    Complete setup

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import './App.css';
 
 function App() {
   const [user, setUser] = useState({});
+
   const handleGoogleCallbackResponse = (response) => {
     console.log('Encoded JWT ID token: ' + response.credential);
     var userObject = jwtDecode(response.credential);
     //TODO USE REDUX TO SET USER
     setUser(userObject);
     console.log(userObject);
+    document.getElementById('sign-in-btn').hidden = true;
+  };
+
+  const handleSignOut = () => {
+    setUser({});
+    document.getElementById('sign-in-btn').hidden = false;
   };
 
   useEffect(() => {
-    /*global google*/
+    /* global google */
     google.accounts.id.initialize({
       client_id:
         '956329766193-cdgv0o9gv6vtf1o1166l0fhnf72hudg4.apps.googleusercontent.com',
@@ -34,11 +41,14 @@ function App() {
         <button id='sign-in-btn'></button>
         {user && (
           <div>
-            <img src={user.picture} alt='Profile pic' />
+            <img src={user.picture} />
             <h4>{user.name}</h4>
           </div>
         )}
       </div>
+      {Object.keys(user).length !== 0 && (
+        <button onClick={handleSignOut}>sign out</button>
+      )}
     </div>
   );
 }
